2c0cd906f5c6be67d0a43d0aff1b866bfccd4370
Ge0frey <jeffkimzyjoe@gmail.com>
2024-11-25 23:04:56 +0300

    Set up a running server at a specified port

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req,res) => {
+    res.send("Hello from the server")
+})
+
+
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`App is listening at port ${PORT}`);
+})
