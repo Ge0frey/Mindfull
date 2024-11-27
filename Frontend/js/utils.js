cc1dcf24faf434d8cfb3c1d9c1e687ae787a5ea4
Ge0frey <jeffkimzyjoe@gmail.com>
2024-11-27 23:37:26 +0300

    flexible API request utility that simplifies making HTTP requests to a backend server

---
+++ Frontend/js/utils.js
@@ -0,0 +1,41 @@
+const BASE_URL = '/api';
+
+export const api = {
+    async request(url, method = 'GET', data = null) {
+        const options = {
+            method,
+            headers: {
+                'content-type':'application/json',
+            },
+        };
+
+        if(data) {
+            options.body = JSON.stringify(data);
+        }
+
+        const response = await fetch(`${BASE_URL}${url}`, options);
+
+        if (!response.ok) {
+            const errorData = await response.json();
+            throw new Error (errorData.message || 'An error occured');
+        }
+
+        return await response.json();
+    },
+
+    get(url) {
+        return this.request(url);
+    },
+
+    post(url,data) {
+        return this.request(url, 'POST'. data);
+    },
+
+    put(url) {
+        return this.request(url, 'PUT', data);
+    },
+
+    delete(url) {
+        return this.request(url, 'DELETE');
+    },
+};
