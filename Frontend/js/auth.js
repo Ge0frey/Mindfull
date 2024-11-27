d80aa932eec6508a65fc93340840949efb6a6b1c
Ge0frey <jeffkimzyjoe@gmail.com>
2024-11-28 00:05:58 +0300

    user authentication logic

---
+++ Frontend/js/auth.js
@@ -0,0 +1,58 @@
+import { api } from "./utils";
+
+export async function login (email, password) {
+    try {
+        const response = await api.post('/auth/login', {
+            email, password
+        });
+        const user = {
+            id: response.user.id,
+            firstName: response.user.first_name,
+            lasttName: response.user.last_name,
+            name: `${response.user.first_name} ${response.user.last_name}`,
+            email: response.user.email
+        };
+        localStorage.setItem('user',JSON.stringify(user));
+        renderHeader();
+        return response;
+    } catch (error) {
+        console.error('Login error:', error);
+        throw new Error(error.message || 'Login failed');
+    }
+}
+
+export async function register(firstName, lastName, email, phone, dateOfBirth, gender, password) {
+    try {
+        const response = await api.post('/api/register', {
+            firstName, 
+            lastName, 
+            email, 
+            phone, 
+            dateOfBirth, 
+            gender, 
+            password
+        });
+        return response.data;
+    } catch (error) {
+        if (error.response) {
+          throw new Error(error.response.data.message || 'Registration failed');
+        } else if (error.request) {
+          throw new Error('No response from server');
+        } else {
+          throw new Error('Error setting up request: ' + error.message);
+        }
+    }
+}
+
+export function logout() {
+    localStorage.removeItem('used');
+    renderHeader();
+}
+
+export function getCurrentUser() {
+    return JSON.parse(localStorage.getItem('user'));
+}
+
+export function isLoggedIn() {
+    return !!getCurrentUser();
+}
